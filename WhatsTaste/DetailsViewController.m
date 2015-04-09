124f8d9b3868b8809710ac63ef6652c753fab3c6
Jayce Yang <Jayce.Yang@me.com>
2015-04-09 10:11:53 +0800

    Use javascript core instead

---
+++ WhatsTaste/DetailsViewController.m
@@ -0,0 +1,39 @@
+//
+//  DetailsViewController.m
+//  WhatsTaste
+//
+//  Created by Jayce Yang on 15/4/9.
+//  Copyright (c) 2015年 DJI. All rights reserved.
+//
+
+#import "DetailsViewController.h"
+
+@interface DetailsViewController ()
+
+@end
+
+@implementation DetailsViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    
+    self.view.backgroundColor = [UIColor lightGrayColor];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
---
+++ WhatsTaste/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  WhatsTaste
+//
+//  Created by Jayce Yang on 15/4/9.
+//  Copyright (c) 2015年 DJI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
