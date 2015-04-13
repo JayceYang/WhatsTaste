59b142ae03af4bacd687b364fb9de222374639c0
Jayce Yang <Jayce.Yang@me.com>
2015-04-13 18:00:54 +0800

    Delete some files

--- WhatsTaste/ViewController.h
+++ WhatsTaste/ViewController.h
@@ -2,12 +2,23 @@
 //  ViewController.h
 //  WhatsTaste
 //
-//  Created by Jayce Yang on 15/4/9.
+//  Created by Jayce Yang on 15/4/2.
 //  Copyright (c) 2015年 DJI. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
+#import <JavaScriptCore/JavaScriptCore.h>
+#import "WebViewController.h"
 
-@interface ViewController : UIViewController
+@protocol TestJSExport<JSExport>
+JSExportAs
+(calculateForJS  /** handleFactorialCalculateWithNumber 作为js方法的别名 */,
+ - (void)handleFactorialCalculateWithNumber:(NSNumber *)number
+ );
 
+@end
+
+
+
+@interface ViewController : WebViewController<TestJSExport>
 @end
